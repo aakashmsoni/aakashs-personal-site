8feb6131f546c435057e078069c472ea2db435cb
Aakash Soni <inos92@gmail.com>
2024-01-09 21:58:25 -0600

    initial commit

---
+++ src/pages/projects/index.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import Layout from '../../components/Layout'
+
+function Projects() {
+  return (
+    <Layout>
+      <div>  
+        <h2>Portfolio</h2>
+        <h3>Projects & Websites I've Created</h3>
+      </div>
+    </Layout>
+  )
+}
+export default Projects;
