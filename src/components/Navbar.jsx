6fe0f47cbe1e4fbb2acebab0ec6533bed6ba5669
Aakash Soni <inos92@gmail.com>
2024-01-11 17:11:11 -0600

    added icon images for github/linkedin, set up links in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import "../styles/navbar.css"
+import { StaticImage } from "gatsby-plugin-image"
 
 export const Navbar = () => {
   return (
@@ -8,6 +9,13 @@ export const Navbar = () => {
         <div className="links">
           <Link to="/">Home</Link>
           <Link to="/projects">Projects</Link>
+          <Link to="">Resume</Link> 
+          <a href="https://www.linkedin.com/in/aakashmsoni/">
+            <StaticImage src="../images/linkedin.png" alt="linkedin" width={22} />
+          </a>
+          <a href="https://www.github.com/aakashmsoni/">
+            <StaticImage src="../images/github.png" alt="linkedin" width={22} />
+          </a>
         </div>
     </nav>
     
