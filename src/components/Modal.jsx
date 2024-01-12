16338fd1050355aedd88f82e8fd188bd643c887d
Aakash Soni <inos92@gmail.com>
2024-01-11 18:29:46 -0600

    created Modal component

---
+++ src/components/Modal.jsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import "../styles/modal.css";
+
+export function Modal(props) {
+  if (props.show) {
+    return (
+      <div className="modal-background">
+        <section className="modal-main">
+          {props.children}
+          <button className="close" type="button" onClick={props.onClose}>
+            &#x2715;
+          </button>
+        </section>
+      </div>
+    );
+  }
+}
