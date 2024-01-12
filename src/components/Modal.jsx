3b5878ae7681bf63ee8a53016acd48a98009897f
Aakash Soni <inos92@gmail.com>
2024-01-11 21:48:40 -0600

    updated modal to allow exit on background click

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,10 +1,12 @@
 import * as React from 'react'
 import "../styles/modal.css";
 
+
+
 export function Modal(props) {
   if (props.show) {
     return (
-      <div className="modal-background">
+      <div className="modal-background" onClick={props.onClose}>
         <section className="modal-main">
           {props.children}
           <button className="close" type="button" onClick={props.onClose}>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,6 +12,7 @@ export function Navbar() {
     setIsShowResumeVisible(false);
   }
 
+  
 
   return (
     <nav>
