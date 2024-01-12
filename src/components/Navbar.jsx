5e360c4edc2157a10445eef2f15b6936c8f7ab10
Aakash Soni <inos92@gmail.com>
2024-01-11 18:30:24 -0600

    set up React hooks to display embedded resume via CloudPDF

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,26 @@ import * as React from "react"
 import { Link } from "gatsby"
 import "../styles/navbar.css"
 import { StaticImage } from "gatsby-plugin-image"
+import { useState, useRef, useEffect } from "react"
+import { Modal } from "./Modal"
 
-export const Navbar = () => {
+export function Navbar() {
+  const [isShowResumeVisible, setIsShowResumeVisible] = useState(false);
+
+  const handleClose = () => {
+    setIsShowResumeVisible(false);
+  }
+
+
   return (
     <nav>
         <div className="links">
           <Link to="/">Home</Link>
           <Link to="/projects">Projects</Link>
-          <Link to="">Resume</Link> 
+          <Link to="" onClick={() => setIsShowResumeVisible(true)}>Resume</Link> 
+          <Modal show={isShowResumeVisible} onClose={handleClose}>
+            <iframe src="https://cloudpdf.io/view/dbDdm_xHs" style={{width:750, height:650}} frameborder="0"></iframe>
+          </Modal>
           <a href="https://www.linkedin.com/in/aakashmsoni/">
             <StaticImage src="../images/linkedin.png" alt="linkedin" width={22} />
           </a>
