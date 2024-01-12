97464802f88e598d28a4eb71705cab9c8e99d81a
Aakash Soni <inos92@gmail.com>
2024-01-12 14:46:43 -0600

    used graphql to add project names & create links to project page

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -1,15 +1,43 @@
 import * as React from 'react'
 import Layout from '../../components/Layout'
 import * as styles from "../../styles/projects.module.css"
+import { graphql } from 'gatsby'
+import { Link } from 'gatsby'
 
-function Projects() {
+function Projects({ data }) {
+  console.log(data)
+  const projects = data.projects.nodes
+
   return (
     <Layout>
       <div className={styles.portfolio}>  
         <h2>Portfolio</h2>
         <h3>Projects & Websites I've Created</h3>
+        <div className={styles.projects}>
+          {projects.map(project => (
+            <Link className="link" to={"/projects" + project.frontmatter.slug} key={project.id}>
+              <h3>{project.frontmatter.title}</h3>
+              <p>{project.frontmatter.stack}</p>
+            </Link>
+          ))}
+        </div>
       </div>
     </Layout>
   )
 }
 export default Projects;
+
+export const query = graphql`
+query MyQuery {
+  projects: allMarkdownRemark {
+    nodes {
+      frontmatter {
+        title
+        stack
+        slug
+      }
+      id
+    }
+  }
+}
+`
