e90e089ee551ccbaaa628b5fac9c75c8e211a4ff
Aakash Soni <inos92@gmail.com>
2024-01-17 16:02:54 -0600

    added more styling classes to profile section

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,13 +6,15 @@ import * as styles from "../styles/home.module.css";
 function Index() {
   return (
     <Layout>
-      <section className={styles.header}>
-        <div className={styles.titles}>
+      <section id="profile" className={styles.profile}>
+        <div className={styles.profileText}>
           <h2>Aakash Soni</h2>
           <h3>Software Engineer</h3>
           <h3>Web Developer</h3>
         </div>
-        <StaticImage src="../images/profile_pic6.png" className="profile_pic" alt="pic" />
+        <div className={styles.profilePicContainer}>
+        <StaticImage src="../images/profile_pic6.png" className="profile_pic" alt="pic" /> 
+        </div>
       </section>
     </Layout>
   );
