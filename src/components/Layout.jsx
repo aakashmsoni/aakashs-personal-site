68fd998c23150c559937efda62ab1310b4bacc67
Aakash Soni <inos92@gmail.com>
2024-01-17 11:17:33 -0600

    minor updates to code

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,12 @@
 import * as React from "react";
 import { Navbar } from "./Navbar";
-import "../styles/global.css"
+import "../styles/global.css";
 
-export default function Layout({children}) {
+export default function Layout({ children }) {
   return (
     <div className="layout">
       <Navbar />
-      <div className="content">
-        { children }
-      </div>
+      <div className="content">{children}</div>
     </div>
-  )
+  );
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,26 +1,23 @@
-import * as React from "react"
+import * as React from "react";
 import Layout from "../components/Layout";
 import { StaticImage } from "gatsby-plugin-image";
-import * as styles from '../styles/home.module.css';
-
+import * as styles from "../styles/home.module.css";
 
 function Index() {
   return (
     <Layout>
-    <section className={styles.header}>
+      <section className={styles.header}>
         <div className={styles.titles}>
-          <h2>
-            Aakash Soni
-          </h2>
+          <h2>Aakash Soni</h2>
           <h3>Software Engineer</h3>
           <h3>Web Developer</h3>
         </div>
         <StaticImage src="../images/profile_pic6.png" className="profile_pic" alt="pic" />
-    </section>
+      </section>
     </Layout>
-  )
+  );
 }
 
-export default Index
+export default Index;
 
-export const Head = () => <title>Aakash Soni</title>
+export const Head = () => <title>Aakash Soni</title>;
