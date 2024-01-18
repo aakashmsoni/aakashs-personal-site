48d387a5fbc2ea4b4ad699df342ed72467efb472
Aakash Soni <inos92@gmail.com>
2024-01-17 22:59:24 -0600

    intiatiated navbar change when scrolling down

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,28 @@ import { Modal } from "./Modal"
 export function Navbar() {
   const [isShowResumeVisible, setIsShowResumeVisible] = useState(false);
 
+  const [nav, showNav] = useState(false);
+  console.log(nav);
+
+  // function toggleOpen(nav) {
+  //   var transformLinks = "translate(100%)"
+  //   if (nav === "true") {
+  //     transformLinks = "translateX(100%)";
+  //   } else {
+  //     transformLinks = "translate(0)";
+  //   }
+  // }
+
+  // function myFunction() {
+  //   var x = document.getElementById("hamburgerLinks");
+  //   if (x.style.display === "block") {
+  //     x.style.display = "none";
+  //   } else {
+  //     x.style.display = "block";
+  //   }
+  // }
+  
+
   const handleClose = () => {
     setIsShowResumeVisible(false);
   }
@@ -21,7 +43,18 @@ export function Navbar() {
     document.getElementById("navbar").style.top = "-50px";
   }
   prevScrollpos = currentScrollPos;
+
+  scrollFunction();
   }
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+      document.getElementById("hamburgerIcon").style.right = "20px";} else {
+        document.getElementById("hamburgerIcon").style.right = "-50px";
+        showNav(false);
+      }
+    }
+  
 
   return (
     <nav id="navbar">
@@ -39,6 +72,17 @@ export function Navbar() {
           <a href="https://www.github.com/aakashmsoni/">
             <StaticImage src="../images/github.png" alt="linkedin" width={22} className="icons" />
           </a>
+          <button id="hamburgerIcon" className="hamburgerIcon" onClick={() => showNav(!nav)}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+          <div id="hamburgerLinks" className="hamburgerLinks" style={nav === true ? {transform: "translateX(0)"} : {transform: "translateX(100%)"}}>
+            <Link to="/" >Home</Link>
+            <Link to="#about" >About</Link>
+            <Link to="/projects" >Projects</Link>
+            <Link to="" onClick={() => setIsShowResumeVisible(true)}>Resume</Link> 
+          </div>
         </div>
     </nav>
     
