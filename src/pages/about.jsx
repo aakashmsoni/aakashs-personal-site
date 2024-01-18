9e57b35a2e067d58388658b9c59f8ac81c57b944
Aakash Soni <inos92@gmail.com>
2024-01-18 12:09:01 -0600

    updated text in about me section

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -6,7 +6,8 @@ export function About () {
     <section id="about" >
       <div className={styles.about}>
         <h2>About Me</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p> I'm a Software Engineer based out of Chicago and a graduate of Actualize Coding Bootcamp. I began learning about code at a young age but it wasn't until recently that I decided to develop my passion into a career in technology. I've always had an affinity for problem-solving and there's no better feeling than when a solution comes together! My motto is that every new problem leads to a new lesson and I love applying those lessons in my personal projects</p>
+        <p>When I'm not coding, you can find me on the beach playing volleyball or way outside of the city, camping underneath the stars. I'm a big sports fan and after having been to championship parades for both the Cubs and the Blackhawks, I'm hoping the next one will be for my Chicago Bulls 🤞</p>
       </div>
     </section>
   )
