726a6ac1dbf272efd9c6adb234e7d18d9b241438
Aakash Soni <inos92@gmail.com>
2024-01-22 12:05:36 -0600

    minor updates to projects section

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -1,17 +1,35 @@
 import * as React from 'react'
-import Layout from '../../components/Layout'
 import * as styles from "../../styles/projects.module.css"
-import { graphql } from 'gatsby'
+import { graphql, useStaticQuery } from 'gatsby'
 
 
-function Projects({ data }) {
+function Projects() {
+const data = useStaticQuery(graphql`
+query ProjectsQuery {
+  projects: allMarkdownRemark {
+    nodes {
+      frontmatter {
+        title
+        stack
+        slug
+        github {
+          frontend
+          backend
+        }
+        type
+      }
+      id
+    }
+  }
+}
+`)
+
   const projects = data.projects.nodes
   console.log(projects)
 
  
 
   return (
-    <Layout>
       <section id="projects">
       <div className={styles.portfolio}>  
         <h2>Portfolio</h2>
@@ -33,27 +51,26 @@ function Projects({ data }) {
         </div>
       </div>
       </section>
-    </Layout>
   )
 }
 export default Projects;
 
-export const query = graphql`
-query ProjectsQuery {
-  projects: allMarkdownRemark {
-    nodes {
-      frontmatter {
-        title
-        stack
-        slug
-        github {
-          frontend
-          backend
-        }
-        type
-      }
-      id
-    }
-  }
-}
-`
+// export const query = graphql`
+// query ProjectsQuery {
+//   projects: allMarkdownRemark {
+//     nodes {
+//       frontmatter {
+//         title
+//         stack
+//         slug
+//         github {
+//           frontend
+//           backend
+//         }
+//         type
+//       }
+//       id
+//     }
+//   }
+// }
+// `
