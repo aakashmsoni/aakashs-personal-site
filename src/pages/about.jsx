c2eef1d8d3c9d13fad89f94493c0a64dd01b0a86
Aakash Soni <inos92@gmail.com>
2024-01-26 10:52:10 -0600

    updated About section, updated styling tohave it on same viewport as Skills

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -6,8 +6,10 @@ function About () {
     <section id="about" >
       <div className={styles.about}>
         <h2>About Me</h2>
-        <p> I'm a Software Engineer based out of Chicago and a graduate of Actualize Coding Bootcamp. I began learning about code at a young age but it wasn't until recently that I decided to develop my passion into a career in technology. I've always had an affinity for problem-solving and there's no better feeling than when a solution comes together! My motto is that every new problem leads to a new lesson and I love applying those lessons in my personal projects</p>
-        <p>When I'm not coding, you can find me playing volleyball (indoor and beach), snowboarding in Wisconsin or camping out under the stars. I'm a big sports fan and after having been to championship parades for both the Cubs and the Blackhawks, I'm hoping the next one will be for my Chicago Bulls 🤞</p>
+        <div className={styles.descriptionContainer}>
+        <p> I'm a Software Engineer based out of Chicago and a graduate of Actualize Coding Bootcamp. I began learning about code at a young age but it wasn't until recently that I decided to develop my passion into a career in technology. I've always had an affinity for problem-solving and there's no better feeling than when a solution comes together! My motto is that every new problem leads to a new lesson and I love applying those lessons in my personal projects.</p><br />
+        <p>When I'm not coding, you can find me playing volleyball (indoor and beach), snowboarding in Wisconsin or camping out under the stars. I'm a big sports fan and after having been to championship parades for both the Cubs and the Blackhawks, I'm hoping the next one will be for the Chicago Bulls 🤞</p>
+        </div>
       </div>
     </section>
   )
