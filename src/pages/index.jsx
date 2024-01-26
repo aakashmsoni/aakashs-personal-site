1677053a819e29542bc2b49f9b8ef9af74b7d0ca
Aakash Soni <inos92@gmail.com>
2024-01-25 23:45:38 -0600

    refactored index to import Profile code and keep all parts of site as separate components

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,32 +1,14 @@
 import * as React from "react";
 import Layout from "../components/Layout";
-import { StaticImage } from "gatsby-plugin-image";
-import * as styles from "../styles/home.module.css";
 import About from "./about";
 import Skills from "./skills";
 import Projects from "./projects";
+import Profile from "./profile";
 
 function Index() {
   return (
     <Layout>
-      <section id="profile" className={styles.profile}>
-        <div className={styles.profileText}>
-          <h2>Aakash Soni</h2>
-          <h3>Software Engineer</h3>
-          <h3>Web Developer</h3>
-          <div className={styles.socialLinks}>
-            <a href="https://www.linkedin.com/in/aakashmsoni/">
-              <StaticImage src="../images/linkedin.png" alt="linkedin" className="icons" />
-            </a>
-            <a href="https://www.github.com/aakashmsoni/">
-              <StaticImage src="../images/github.png" alt="linkedin" className="icons" />
-            </a>
-          </div>
-        </div>
-        <div className={styles.profilePicContainer}>
-        <StaticImage src="../images/profile_pic6.png" className="profile_pic" alt="pic" /> 
-        </div>
-      </section>
+      <Profile id="profile" />
       <About id="about" />
       <Skills id="skills" />
       <Projects id="projects" />
