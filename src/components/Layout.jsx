ee8430a4ea5d17166804390223dbb05f4dfc3ece
Aakash Soni <inos92@gmail.com>
2024-01-26 10:52:49 -0600

    imported Footer into main Layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Navbar } from "./Navbar";
+import { Footer } from "./Footer";
 import "../styles/global.css";
 
 export default function Layout({ children }) {
@@ -7,6 +8,7 @@ export default function Layout({ children }) {
     <div className="layout">
       <Navbar />
       <div className="content">{children}</div>
+      <Footer />
     </div>
   );
 }
