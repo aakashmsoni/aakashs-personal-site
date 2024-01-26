e53bf0e6e77c42c775b0f35cd935ce785ea8fb8a
Aakash Soni <inos92@gmail.com>
2024-01-26 10:44:23 -0600

    created Footer & styling

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import "../styles/footer.css"
+
+export function Footer() {
+  return (
+    <section id="footer">
+      <div>
+        <p>Copyright 2024 Aakash Soni</p>
+      </div>
+    </section>
+  )
+}
