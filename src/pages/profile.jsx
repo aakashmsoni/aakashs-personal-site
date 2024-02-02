fd2ea2311465c479e385105270349eaa0f05cf17
Aakash Soni <inos92@gmail.com>
2024-02-02 13:15:37 -0600

    minor updates

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -12,11 +12,11 @@ function Profile() {
           <div className={styles.socialLinks}>
             <a href="https://www.linkedin.com/in/aakashmsoni/">
               <StaticImage src="../images/linkedin.png" alt="linkedin" className="icons" layout="constrained" style={{ width: "40px",
-  height: "40px", margin: "10px", overflow: "visible" }}/>
+              height: "40px", margin: "10px", overflow: "visible" }}/>
             </a>
             <a href="https://www.github.com/aakashmsoni/">
               <StaticImage src="../images/github.png" alt="github" className="icons" layout="constrained" style={{ width: "40px",
-  height: "40px", margin: "10px", overflow: "visible" }}/>
+              height: "40px", margin: "10px", overflow: "visible" }}/>
             </a>
           </div>
         </div>
